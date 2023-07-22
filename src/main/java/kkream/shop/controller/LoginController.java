a90226c9d9e6bbcf36b834a3c25c430f236ce075
donghyunkim <ghffu405@gmail.com>
2023-07-22 16:28:13 +0900

    design: login.html 생성 및 login 컨트롤러 생성후 렌더링 확인

---
+++ src/main/java/kkream/shop/controller/LoginController.java
@@ -0,0 +1,19 @@
+package kkream.shop.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/login")
+public class LoginController {
+
+    @GetMapping
+    public String login(){
+        return "login";
+    }
+
+
+}
